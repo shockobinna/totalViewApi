09b57df6a688d8966f5ef6cd55c7aa6fac82d12e
Lekwuwa <davidokorie59@gmail.com>
2025-03-10 20:24:36 -0300

    Add load file template

--- upLoadExcel/loadLoginLogout/urls.py
+++ upLoadExcel/loadLoginLogout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.loadData, name='home'),
+    path('', views.queryDataBase, name='home'),
+    path('upload_excel/', views.upload_excel, name='uploadExcel'),
 ]
--- upLoadExcel/loadLoginLogout/views.py
+++ upLoadExcel/loadLoginLogout/views.py
@@ -8,22 +8,29 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-def loadData(request):
-    template = loader.get_template('homepage.html')
-    return HttpResponse(template.render())
+def queryDataBase(request):
+    if request.method == 'POST':
+        
+    # template = loader.get_template('homepage.html')
+    
+        # Retrieve the date fields from the request
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+
+        print(f"start date: {start_date}, end date: {end_date}")
+    return render(request, 'homepage.html')
     # return HttpResponse("Hello world!")
 
 
 def upload_excel(request):
     if request.method == 'POST' and request.FILES.get('excel_file'):
         excel_file = request.FILES['excel_file']
+        if excel_file:
+            print(excel_file)
 
-        # Retrieve the date fields from the request
-        start_date = request.POST.get('start_date')
-        end_date = request.POST.get('end_date')
 
-        num_records = load_data_from_excel(excel_file)
-        return render(request, 'result.html', {'record_count': num_records})
-    return render(request, 'homepage.html')
+        # num_records = load_data_from_excel(excel_file)
+        # return render(request, 'result.html', {'record_count': num_records})
+    return render(request, 'upLoadExcel.html')
 
 
